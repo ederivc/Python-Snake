61a05156ff20e5e3254f9a445b0308150a09dbd5
ederivc <61289461+ederivc@users.noreply.github.com>
2020-08-16 18:05:08 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,120 +1,73 @@
-import sys
-import time
-import pygame
 
-class Snake:
-    def __init__(self):
-        self.velocity = 5
-        self.x = 200
-        self.y = 200
-        self.snake_list = [200, 200]
-        self.snake_body = [[200, 200]]
-        self.direction = "right"
-
-    def draw(self, window):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    self.x -= self.velocity
-                    #self.increment_velocity()
-                    self.direction = "left"
-                    self.update_display(window)
-
-                if event.key == pygame.K_RIGHT:
-                    self.x += self.velocity
-                    #self.increment_velocity()
-                    self.direction = "right"
-                    self.update_display(window)
-
-                if event.key == pygame.K_DOWN:
-                    self.snake_list[1] += self.velocity
-                    #self.increment_velocity()
-                    self.direction = "down"
-                    self.update_display(window)
-
-                if event.key == pygame.K_UP:
-                    self.y -= self.velocity
-                    #self.increment_velocity()
-                    self.direction = "up"
-                    self.update_display(window)
-
-
-        if self.direction == "right":
-            x = self.snake_body[0][0] + self.velocity
-            self.snake_body.insert(0, [x, self.snake_body[0][1]])
-            self.snake_body.pop()
-            window.fill(WHITE)
-            self.update_display(window)
-
-        if self.direction == "left":
-            x = self.snake_body[0][0] - self.velocity
-            self.snake_body.insert(0, [x, self.snake_body[0][1]])
-            self.snake_body.pop()
-            window.fill(WHITE)
-            self.update_display(window)
-
-        if self.direction == "down":
-            y = self.snake_body[0][1] + self.velocity
-            self.snake_body.insert(0, [self.snake_body[0][0], y])
-            self.snake_body.pop()
-            window.fill(WHITE)
-            self.update_display(window)
-
-        if self.direction == "up":
-            y = self.snake_body[0][1] - self.velocity
-            self.snake_body.insert(0, [self.snake_body[0][0], y])
-            self.snake_body.pop()
-            window.fill(WHITE)
-            self.update_display(window)
-
-
-    def update_display(self, window):
-        var1 = 20
-        var2 = 20
-        pygame.draw.rect(window, BLACK, [100, 100, 20, 20])
-        
-        pygame.draw.rect(window, BLACK, [100, 100, 20, 20])
-        for body in self.snake_body:
-
-
-            pygame.draw.rect(window, BLACK, [body[0], body[1], var1, var2])
-            pygame.display.update()
-
-        clock.tick(60)
-
-
-    def increment_velocity(self):
-        self.velocity += 0.5
-
-
-WIDTH = 900
-HEIGHT = 800
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-
-grid_size = 20
-grid_width = WIDTH/grid_size
-grid_height = HEIGHT/grid_size 
-clock = pygame.time.Clock()
-
-
-def main():
-    pygame.init()
-    window = pygame.display.set_mode((WIDTH, HEIGHT))
-    window.fill(WHITE)
-    pygame.display.set_caption("First Game")
-    pygame.display.flip()
-    
-    snake = Snake()
-
-    while True:
-        snake.draw(window)
-        pygame.display.update()
-        #pygame.display.flip()
-
-main()
+import pygame 
+import sys 
+  
+  
+# initializing the constructor 
+pygame.init() 
+  
+# screen resolution 
+res = (720,720) 
+  
+# opens up a window 
+screen = pygame.display.set_mode(res) 
+  
+# white color 
+color = (255,255,255) 
+  
+# light shade of the button 
+color_light = (170,170,170) 
+  
+# dark shade of the button 
+color_dark = (100,100,100) 
+  
+# stores the width of the 
+# screen into a variable 
+width = screen.get_width() 
+  
+# stores the height of the 
+# screen into a variable 
+height = screen.get_height() 
+  
+# defining a font 
+smallfont = pygame.font.SysFont('Corbel',35) 
+  
+# rendering a text written in 
+# this font 
+text = smallfont.render('quit' , True , color) 
+  
+while True: 
+      
+    for ev in pygame.event.get(): 
+          
+        if ev.type == pygame.QUIT: 
+            pygame.quit() 
+              
+        #checks if a mouse is clicked 
+        if ev.type == pygame.MOUSEBUTTONDOWN: 
+              
+            #if the mouse is clicked on the 
+            # button the game is terminated 
+            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
+                pygame.quit() 
+                  
+    # fills the screen with a color 
+    screen.fill((60,25,60)) 
+      
+    # stores the (x,y) coordinates into 
+    # the variable as a tuple 
+    mouse = pygame.mouse.get_pos() 
+      
+    # if mouse is hovered on a button it 
+    # changes to lighter shade  
+    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
+        pygame.draw.rect(screen,color_light,[width/2,height/2,140,40]) 
+          
+    else: 
+        pygame.draw.rect(screen,color_dark,[width/2,height/2,140,40]) 
+      
+    # superimposing the text onto our button 
+    screen.blit(text , (width/2+50,height/2)) 
+      
+    # updates the frames of the game 
+    pygame.display.update() 
--- test.py
+++ test.py
@@ -1,39 +1,77 @@
+import sys
 import random
 import pygame
+from tkinter import messagebox as mBox
 
-WIDTH = 800
-HEIGHT = 800
+WIDTH = 500
+HEIGHT = 500
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+BLUE = (59, 59, 198)
+PURPLE = (132, 68, 201)
 GRID_SIZE = 20
+pygame.font.init() # you have to call this at the start, 
+                   # if you want to use this module.
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+textsurface = myfont.render('Some Text', False, (0, 0, 0))
 
+class Food:
+    def __init__(self):
+        self.position = (0, 0)
+        self.random_position()
+
+    def random_position(self):
+        self.position = (random.randrange(0, 500, 20), random.randrange(0, 500, 20))
+
+    def draw_food(self, window):
+        pygame.draw.rect(window, WHITE, [self.position[0], self.position[1] , 20, 20])
+        #print(self.random_position)
+        pygame.display.update()
+        
 
 class Snake:
     def __init__(self):
         self.velocity = 20
-        self.snake_body = [[0,0], [0,1], [0,2], [0, 3]]
-        self.random_movement()
-        self.actual_movement = "right"
-        self.turn = " "
+        self.snake_body = [[220,220], [220,240], [220,260], [220, 280]]
+        self.actual_movement = random.choice(["right", "left", "up", "down"])
+        self.incorrect_movements = {"right": ["left"],
+                                    "left": ["right"],
+                                    "up": ["down"],
+                                    "down": ["up"]}
+        self.get_snake_head()
 
     def move_snake(self, window):
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
             elif event.type == pygame.KEYDOWN:
+
                 if event.key == pygame.K_LEFT:
+                    if self.check_valid_movement(pygame.key.name(event.key)):
+                        continue
+
                     self.actual_movement = "left"
 
                 if event.key == pygame.K_RIGHT:
+                    if self.check_valid_movement(pygame.key.name(event.key)):
+                        continue
+
                     self.actual_movement = "right"
 
                 if event.key == pygame.K_DOWN:
+                    if self.check_valid_movement(pygame.key.name(event.key)):
+                        continue
+
                     self.actual_movement = "down"
 
                 if event.key == pygame.K_UP:
+                    if self.check_valid_movement(pygame.key.name(event.key)):
+                        continue
+
                     self.actual_movement = "up"
 
 
@@ -43,37 +81,43 @@ class Snake:
     def draw_snake(self, window):
         for body in self.snake_body:
             pygame.draw.rect(window, RED, [body[0], body[1], 20, 20])
+
+        self.check_error(window)
+
         pygame.display.update()
 
 
+    def check_error(self, window):
+        if list(self.get_snake_head()) in self.snake_body[2:]:
+            print("You lost")
+            window.blit(textsurface,(0,0))
+            pygame.time.delay(1000)
+            self.reset()
 
-    def check_movement(self, window, movement):
-        for i, value in enumerate(self.snake_body):
-            self.snake_body[i][1] += self.velocity
 
-        print(self.snake_body)
-        self.draw_snake(window)
-
-
-    def random_movement(self):
-        pass
-
+    def reset(self):
+        self.snake_body = [[220,220], [220,240], [220,260], [220, 280]]
+        self.actual_movement = random.choice(["right", "left", "up", "down"]) 
 
     def snake_movements(self, window):
         if self.actual_movement == "right":
             x = self.snake_body[0][0] + self.velocity
+            x = self.check_border(x, "max_limit")
             self.update_snake(x, "x", window)
 
         if self.actual_movement == "left":
             x = self.snake_body[0][0] - self.velocity
+            x = self.check_border(x, "lower_limit")
             self.update_snake(x, "x", window)
 
         if self.actual_movement == "up":
             y = self.snake_body[0][1] - self.velocity
+            y = self.check_border(y, "lower_limit")
             self.update_snake(y, "y", window)   
 
         if self.actual_movement == "down":
             y = self.snake_body[0][1] + self.velocity
+            y = self.check_border(y, "max_limit")
             self.update_snake(y, "y", window)
  
 
@@ -84,14 +128,39 @@ class Snake:
             redraw_window(window)
             self.draw_snake(window)
 
-        else:
+        elif cons == "x":
             self.snake_body.insert(0, [value, self.snake_body[0][1]])
             self.snake_body.pop()
             redraw_window(window)
             self.draw_snake(window)
 
+        else:
+            print(list(value))
+            self.snake_body.insert(0, list(value))
+            redraw_window(window)
+            self.draw_snake(window)
 
 
+    def check_border(self, checked_value, limit):
+        if limit == "max_limit":
+            if checked_value > 480:
+                return 0
+            else:
+                return checked_value
+        else:
+            if checked_value < 0:
+                return 480
+            else:
+                return checked_value
+
+
+    def check_valid_movement(self, next_mov):
+        if next_mov in self.incorrect_movements[self.actual_movement]:
+            return True
+
+    
+    def get_snake_head(self):
+        return tuple(self.snake_body[0])
 
 
 def draw(window):
@@ -104,9 +173,10 @@ def draw(window):
         pygame.draw.line(window, BLACK, (x, 0), (x, WIDTH))
         pygame.draw.line(window, BLACK, (0, y), (HEIGHT, y))
 
+        
 
 def redraw_window(window):
-    window.fill(WHITE)
+    window.fill(BLUE)
     draw(window)
     pygame.display.update()
 
@@ -116,21 +186,28 @@ def main():
     pygame.init()
 
     window = pygame.display.set_mode((WIDTH, HEIGHT))
-    window.fill(WHITE)
+    window.fill(BLUE)
     pygame.display.set_caption("Snake Game")
 
     clock = pygame.time.Clock()
     snake = Snake()
+    food = Food()
 
     run = True
 
     while run:
-        #pygame.time.delay(50)
-        clock.tick(10)
+        #pygame.time.delay(5)
+        clock.tick(9)
 
         draw(window)
-
         snake.move_snake(window)
 
+        if snake.get_snake_head() == food.position:
+            snake.update_snake(food.position, "w", window)
+            food.random_position()
+            food.draw_food(window)
+
+
+        food.draw_food(window)
 
 main()
